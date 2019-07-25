afe124fcc049b4f6ef1ba801cfb90324fe60daf1
janaki.kunapareddy <janaki.kunapareddy@USABOS1003188L>
2019-07-24 21:59:40 -0500

    commit janakiPractice

--- src/test/java/Practices/locators.java
+++ src/test/java/Practices/locators.java
@@ -1,5 +1,28 @@
 package Practices;
 
-public class locators {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class locators {
+	
+	
+	public static void main (String [] args){
+																		// Open browser
+		WebDriver driver = new ChromeDriver();
+																			// To maximize the window
+		driver.manage().window().maximize();
+																				// Open Application
+		driver.get("https://www.google.co.in/?gws_rd=ssl#q=softwaretestingmaterial.com");
+																		// Get text of a particular link
+		//String FindElement = driver.findElement(By.xpath("//*[@id='rso']/div[1]/div/div/h3/a")).getText();
+		
+													// Print the value of the link
+		//System.out.println(FindElement);
+		//driver.findElement(By.xpath("//*[@id='rso']/div[1]/div/div/h3/a")).click();
+		
+		driver.findElement(By.xpath("//a[contains(text(),'Java Tutorial')]")).click();;
+																
+		}
+		
 }
